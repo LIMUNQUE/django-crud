8242b26e677d2bfc8232be9c5e14fd4fa197e6b9
Joshua Cobos <44756521+LIMUNQUE@users.noreply.github.com>
2022-10-01 18:31:19 -0500

    User register

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
 # Create your views here.
 
+def home(request):
+    return render(request, 'home.html')
+
 def signup(request):
-    return render(request, 'signup.html', {
-        'form': UserCreationForm()
-    })
+    if request.method == 'GET':
+        #ENVIANDO DATOS
+        return render(request, 'signup.html', {
+            'form': UserCreationForm()
+        })
+    else:
+        #RECIVIENDO DATOS
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado correctamente')	
+            except:
+                return HttpResponse('El usuario ya existe')
+        else:
+            return HttpResponse('Las contraseñas no coinciden')
+    
